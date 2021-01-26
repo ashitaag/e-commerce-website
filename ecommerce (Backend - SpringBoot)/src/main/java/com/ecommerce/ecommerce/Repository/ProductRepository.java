7e81576f180e0df63244c46e007f9954a3715d56
Ashita Agarwal <ashita.agarwalkb@gmail.com>
2021-01-26 12:45:54 -0600

    Removed Initial Files

--- ecommerce (Backend - SpringBoot)/src/main/java/com/ecommerce/ecommerce/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.ecommerce.Repository;
-
-import com.ecommerce.ecommerce.model.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Products, String> {
-}
